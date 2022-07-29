6250d9535a9bf4842c350dbb64281eca7aca2143
Tobias Lekman <tobias@lekman.com>
2022-07-29 09:12:22 +0200

    matching brackets

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,15 @@
 def is_paired(input_string):
-    pass
+    
+    op = '' # track opened brackets 
+    matched = lambda c, o: (c == '}' and o == '{') or (c == ')' and o == '(') or (c == ']' and o == '[')
+    
+    for c in input_string:
+        if c in '[({': op += c # append open bracket
+        if c in '])}': # check that bracket matches
+            if len(op) == 0 or not matched(c, op[-1]):
+                return False
+            op = op[:-1]
+  
+    return len(op) == 0
+
+print(is_paired("{[]}"))
